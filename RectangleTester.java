19fb94443509bf9ef3dc593768aec3a1d45dbd59
phippsa-wlhs <phippsa@wlhs.wlwv.k12.or.us>
2022-10-04 09:10:08 -0700

    created two rectangle objects

--- RectangleTester.java
+++ RectangleTester.java
@@ -10,13 +10,16 @@ public class RectangleTester
         //this is a test of GitHub
         
         //Make a rectangle called r1 with a width of 10 and height of 2 then print it
-        
-         
+        Rectangle r1 = new Rectangle(10,2);
+        System.out.println(r1);
+        r1.printArea();
          //print just the area of the rectangle
          
          
          // Rectangle is the class and also the type of this object.
          // Make a new rectangle called r2 with a width of 5 and height of 15, print it, then print just its area
-         
+         Rectangle r2 = new Rectangle(5,15);
+        System.out.println(r2);
+        r2.printArea();
     }
 }
